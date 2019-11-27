7fc53465267af8758e72ac3787e1c9d1e52930b0
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:11 +0300

    Create Unit.cs

---
+++ Unit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public class Unit //done
+    {
+        private readonly String _name;
+        private readonly String _symbol;
+
+        public String Name
+        {
+            get { return _name; }
+        }
+
+        public String Symbol
+        {
+            get { return _symbol; }
+        }
+
+        public Unit(String name, String symbol)
+        {
+            this._name = name;
+            this._symbol = symbol;
+        }
+
+        
+    }
+}
