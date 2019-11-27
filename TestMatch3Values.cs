30e03062f805e1b0fa8f76241f161d4b940d7f91
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:13 +0300

    Create TestMatch3Values.cs

---
+++ TestMatch3Values.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public class TestMatch3Values : TestDescription
+    {
+        public Range3Values standardResult;
+        public TestMatch3Values(string name, Importance category, Range3Values testRange, string unitName, string symbol)
+            :base(name, category, unitName, symbol)
+        {
+            this.standardResult = testRange;
+        }
+        public override bool InRange(double value)
+        {
+            if (value == standardResult.Value1 || value == standardResult.Value2 || value == standardResult.Value3)
+                return true;
+            else
+                return false;
+        }
+    }
+}
