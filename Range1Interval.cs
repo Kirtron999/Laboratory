29623ceadc2b0900f9db59c8c154efcd45be0b72
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:35 +0300

    Create Range1Interval.cs

---
+++ Range1Interval.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public class Range1Interval
+    {
+        private readonly double _min;
+        private readonly double _max;
+
+        public double Min
+        {
+            get { return _min; }
+        }
+
+        public double Max
+        {
+            get { return _max; }
+        }
+
+        public Range1Interval(double min, double max)
+        {
+            if (min > max)
+            {
+                this._min = max;
+                this._max = min;
+            }
+            else
+            {
+                this._min = min;
+                this._max = max;
+            }
+        }
+    }
+}
