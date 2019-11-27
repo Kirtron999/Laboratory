36972676ac282f0cab576e88353c5029f4fa782c
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:31 +0300

    Create Lab.cs

---
+++ Lab.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+public enum Importance
+{
+    usual,
+    important
+}
+
+public enum State
+{
+    reception,
+    research,
+    processing,
+    archive
+}
+
+namespace Laboratory
+{
+    public class Lab 
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+
+            
+        }
+        
+    }
+}
