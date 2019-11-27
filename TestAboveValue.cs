960e08f13c460853a1cc7aafd8af91161cf465ae
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:22 +0300

    Create TestAboveValue.cs

---
+++ TestAboveValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public class TestAboveValue : TestDescription
+    {
+        public Range1Value standardResult;
+        public TestAboveValue(string name, Importance category, Range1Value testRange, string unitName, string symbol)
+            :base(name, category, unitName, symbol)
+        {
+            this.standardResult = testRange;
+        }
+        public override bool InRange(double value)
+        {
+            if (value > standardResult.Value)
+                return true;
+            else
+                return false;
+        }
+    }
+}
