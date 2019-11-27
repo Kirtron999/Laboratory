564cbb27420d430b0773c485fd62dcf19b9c723a
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:15 +0300

    Create TestInInterval.cs

---
+++ TestInInterval.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public class TestInInterval : TestDescription
+    {
+        public Range1Interval standardResult;
+        public TestInInterval(string name, Importance category, Range1Interval testRange, string unitName, string symbol)
+            :base(name, category, unitName, symbol)
+        {
+            this.standardResult = testRange;
+        }
+        public override bool InRange(double value)
+        {
+            if (standardResult.Min <= value && value <= standardResult.Max)
+                return true;
+            else
+                return false;
+        }
+    }
+}
