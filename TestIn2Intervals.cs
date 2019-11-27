7b36ff7e46259ee6b150cf635e31b096a4b3140c
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:16 +0300

    Create TestIn2Intervals.cs

---
+++ TestIn2Intervals.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public class TestIn2Intervals : TestDescription
+    {
+        public Range2Intervals standardResult;
+        public TestIn2Intervals(string name, Importance category, Range2Intervals testRange, string unitName, string symbol)
+            :base(name, category, unitName, symbol)
+        {
+            this.standardResult = testRange;
+        }
+        public override bool InRange(double value)
+        {
+            if ((standardResult.Min1 <= value && value <= standardResult.Max1) || (standardResult.Min2 <= value && value <= standardResult.Max2))
+                return true;
+            else
+                return false;
+        }
+    }
+}
