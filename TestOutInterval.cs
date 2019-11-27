ab2a37976cefd6e5e8d4ff310a0aebb1c57f5d27
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:20 +0300

    Create TestOutInterval.cs

---
+++ TestOutInterval.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public class TestOutInterval : TestDescription
+    {
+        public Range1Interval standardResult;
+        public TestOutInterval(string name, Importance category, Range1Interval testRange, string unitName, string symbol)
+            : base(name, category, unitName, symbol)
+        {
+            this.standardResult = testRange;
+        }
+        public override bool InRange(double value)
+        {
+            if (standardResult.Min > value || value > standardResult.Max)
+                return true;
+            else
+                return false;
+        }
+    }
+}
