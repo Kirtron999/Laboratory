54ff65bb2d8e01e86ce0987ab39f1c722da82c0c
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:37 +0300

    Create Range1Value.cs

---
+++ Range1Value.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public class Range1Value
+    {
+        public double Value;
+
+      
+
+        public Range1Value(double value)
+        {
+            this.Value = value;
+        }
+    }
+}
