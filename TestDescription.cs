57d2dde76a7fb99cb8dfd1c469c51520919f79ec
Kirtron999 <53367374+Kirtron999@users.noreply.github.com>
2019-11-28 02:32:18 +0300

    Create TestDescription.cs

---
+++ TestDescription.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory
+{
+    public abstract class TestDescription
+    {
+
+        public string Name;
+        public Importance Category;
+        public Unit Measure;
+
+        public TestDescription(string name, Importance category, string unitName, string symbol)
+        {
+            this.Name = name;
+            this.Category = category;
+            //this.standardResult = testRange;
+            this.Measure = new Unit(unitName, symbol);
+
+        }
+
+        public abstract bool InRange(double value);
+    }
+}
